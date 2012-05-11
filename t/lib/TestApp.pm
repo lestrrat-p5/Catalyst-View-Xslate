37eb6de04d32bd4b690446e615f4a6321ddcb5af
Wallace Reis <wreis@123people.com>
2012-05-11 22:17:35 +0200

    Fix default_view config for TestApp

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -8,7 +8,7 @@ our $VERSION = '0.01';
 __PACKAGE__->config(
     name                  => 'TestApp',
     default_message       => 'hi',
-    default_view          => 'Pkgconfig',
+    default_view          => 'Xslate::Pkgconfig',
 );
 
 __PACKAGE__->setup;
